e4698d5d16c14629f9cc07d69186a1e785bae98d
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-08-07 12:47:49 -0700

    Create 02-strings.js

---
+++ src/es6/02-strings.js
@@ -0,0 +1,14 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+console.log("🚀 ~ file: 02-strings.js:4 ~ epicPhrase:", epicPhrase);
+
+// Template literals
+let epicPhrase2 = `${hello} ${world} !`;
+console.log("🚀 ~ file: 02-strings.js:8 ~ epicPhrase2:", epicPhrase2);
+
+// Multi-line string
+let lorem = `esta es una frase epica
+esta es la continuacion
+y asi consecutivamente `;
+console.log("🚀 ~ file: 02-strings.js:14 ~ lorem:", lorem);
