41986493adb7d62d0a7b607d726ec1d6b8652b1e
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-08-07 13:07:10 -0700

    Update 04-rest-spread.js

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -9,3 +9,10 @@ console.log(a, b);
 let user = { username : 'Oscar', age: 34};
 let {username, age} = user;
 console.log(username, age);
+
+// spread operator
+
+let person = {name: 'Oscar', age: 28};
+let country = 'MX';
+let data = {...person, country};
+console.log(data)
