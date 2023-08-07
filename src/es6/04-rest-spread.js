eb1aca762ecb01af1c85916bd49d0ca895e07601
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-08-07 12:59:57 -0700

    Create 04-rest-spread.js

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,11 @@
+// arrays destructure
+
+let fruits = ['Apple', 'Banana'];
+let [a, b] = fruits;
+console.log(a, b);
+
+// object destructuring
+
+let user = { username : 'Oscar', age: 34};
+let {username, age} = user;
+console.log(username, age);
